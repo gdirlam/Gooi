1459036f942a9ff2d18167a9c8c65a51abada77d
Gareth Dirlam <gdirlam@gmail.com>
2012-10-23 17:07:56 -0400

    strings back

--- GooiAssert/GooiAssert.js
+++ GooiAssert/GooiAssert.js
@@ -1,11 +1,25 @@
 ﻿var Gooi = Gooi || {};
 //Gooi.Core = Gooi.Core || {};
 
-Gooi.Assert = {
-alert('Init')
+Gooi.Assert = function () {
+    var exp = arguments[0]
+    var message = arguments[1]
+    //var additional = arguments.slice[1]
+    if (!exp) {
+        throw new AssertException(message);
+    }
+    this.AssertException = function (message) {
+        this.message = message
+        this.toString = function () {
+            return 'AssertException: ' + this.message
+        }
+    }
 }
 
+
+/*
 (function GooiAssertPrototype() {
 
 })()
+*/
 
