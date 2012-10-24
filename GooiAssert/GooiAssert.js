d8d1a7f6939fde6bc58d0ad7ffb1935862af6a85
Gareth Dirlam <gdirlam@gmail.com>
2012-10-23 22:05:52 -0300

    Update GooiAssert/GooiAssert.js

--- GooiAssert/GooiAssert.js
+++ GooiAssert/GooiAssert.js
@@ -2,18 +2,21 @@
 //Gooi.Core = Gooi.Core || {};
 
 Gooi.Assert = function () {
-    var exp = arguments[0]
-    var message = arguments[1]
-    //var additional = arguments.slice[1]
-    if (!exp) {
-        throw new this.AssertException(message);
-    }
     this.AssertException = function (message) {
         this.message = message
         this.toString = function () {
             return 'AssertException: ' + this.message
         }
     }
+
+    var exp = arguments[0]
+    var message = arguments[1]
+    //var additional = arguments.slice[1]
+
+    if (!exp) {
+        throw new this.AssertException(message);
+    }
+
 }
 
 
