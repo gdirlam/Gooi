7bc1f51bf66e774aa0b965c39d100c87caa1a751
libra guest <gdirlam@gmail.com>
2012-10-25 07:59:58 -0400

    Loader

--- GooiCore/GooiCoreLoader.js
+++ GooiCore/GooiCoreLoader.js
@@ -2,10 +2,16 @@
 var Gooi = Gooi || {};
 Gooi.Core = Gooi.Core || {};
 
+Gooi.Load = function(){
+    
+}      
+
+
 Gooi.Core.Loader =  ( function (base) {
 
 return base
 }( Gooi.Core.Loader || {} ));       
 
 //debugger; 
+Gooi.Assert.isInstanceOf(Gooi.Load, Object )
 Gooi.Assert.isInstanceOf(Gooi.Core.Loader, Object )
