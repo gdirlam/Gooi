9a1edaea4ba943cdb0d9b3897c6b225bc6e53981
Gareth Dirlam, web developer <gdirlam@gmail.com>
2012-11-07 06:57:53 -0500

    core prototypes for things below js 1.6

--- GooiCore/GooiCore.js
+++ GooiCore/GooiCore.js
@@ -1,14 +1,8 @@
+/*jshint asi:true, supernew:true */
+
 var Gooi = Gooi || {};
 
-(function js16(){
-    if ( !Array.prototype.forEach ) {
-      Array.prototype.forEach = function(fn, scope) {
-        for(var i = 0, len = this.length; i < len; ++i) {
-          fn.call(scope, this[i], i, this);
-        }
-      }
-    }    
-    })()
+
 
 
 Gooi.Core = ( function ( base ) {
@@ -25,7 +19,73 @@ Gooi.Core = ( function ( base ) {
           }
         return destination
     };
-    
+    base.EcmaCompatiblity =  base.EcmaCompatiblity || {}
+    base.EcmaCompatiblity.array = ( base.EcmaCompatiblity.array || {} )
+    base.EcmaCompatiblity.array.forEach = function(){
+            if ( !Array.prototype.forEach ) {
+              Array.prototype.forEach = function(fn, scope) {
+                for(var i = 0, len = this.length; i < len; ++i) {
+                  fn.call(scope, this[i], i, this);
+                }
+              }
+            }                    
+        }
+    base.EcmaCompatiblity.array.map = function(){
+                if (!Array.prototype.map) {
+                  Array.prototype.map = function(callback, thisArg) {
+                    var T, A, k;
+                    if (this == null) {
+                      throw new TypeError(" this is null or not defined");
+                    }
+                    var O = Object(this);
+                    var len = O.length >>> 0;
+                    if ({}.toString.call(callback) != "[object Function]") {
+                      throw new TypeError(callback + " is not a function");
+                    }
+                    if (thisArg) {
+                      T = thisArg;
+                    }
+                    A = new Array(len);
+                    k = 0;
+                    while(k < len) {
+                      var kValue, mappedValue;
+                      if (k in O) {
+                        kValue = O[ k ];
+                        mappedValue = callback.call(T, kValue, k, O);
+                        A[ k ] = mappedValue;
+                      }
+                      k++;
+                    }
+                    return A;
+                  };      
+                }              
+            }       
+    base.EcmaCompatiblity.array.reduce = function(){
+        if (!Array.prototype.reduce) {
+          Array.prototype.reduce = function reduce(accumulator){
+            if (this===null || this===undefined) throw new TypeError("Object is null or undefined");
+            var i = 0, l = this.length >> 0, curr;
+            if(typeof accumulator !== "function") 
+              throw new TypeError("First argument is not callable");
+            if(arguments.length < 2) {
+              if (l === 0) throw new TypeError("Array length is 0 and no second argument");
+              curr = this[0];
+              i = 1; 
+            }
+            else
+              curr = arguments[1];
+            while (i < l) {
+              if(i in this) curr = accumulator.call(undefined, curr, this[i], i, this);
+              ++i;
+            }
+            return curr;
+          };
+        }        
+    }
+    base.EcmaCompatiblity.array.forEach()          
+    base.EcmaCompatiblity.array.map()
+    base.EcmaCompatiblity.array.reduce()
+        
 return base
 }( Gooi.Core || {} ));       
 
